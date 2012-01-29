bd88b09cf1392196f4f0ca21f7393afa4df197e6
10 <1_0@usa.com>
2012-01-29 08:30:53 +0200

    main >> impactuX

--- lib/RawConfigParserGZ.py
+++ lib/RawConfigParserGZ.py
@@ -3,37 +3,27 @@
 #
 #       RawConfigParserGZ.py
 #       RawConfigParserGZ - class to read/write conf ini files from/to gz-archive
+#       Based on RawConfigParser from ConfigParser.py - included in python 2.6 distr.
 #       Copyright 2011 10 <1_0 <at> list.ru>
 #       
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import ConfigParser
 
 class RawConfigParserGZ(ConfigParser.RawConfigParser):
     def read(self, filenames):
         """Read and parse a gz filename or a list of filenames.
-
-        Files that cannot be opened are silently ignored; this is
-        designed so that you can specify a list of potential
-        configuration file locations (e.g. current directory, user's
-        home directory, systemwide directory), and all existing
-        configuration files in the list will be read.  A single
-        filename may also be given.
-
-        Return list of successfully read files.
         """
         import gzip
         #import new_gzip as gzip
